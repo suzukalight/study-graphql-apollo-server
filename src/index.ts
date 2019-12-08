6d78ff033e97d65e01162b21cddc0ca1c1d6b0ac
suzukalight <mail@mkubara.com>
2019-12-08 11:54:20 +0900

    Add user queries

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer, gql, IResolvers } from "apollo-server-express";
 
 const app = express();
 
@@ -9,16 +9,35 @@ app.use(cors());
 const schema = gql`
   type Query {
     me: User
+    users: [User!]
+    user(id: ID!): User
   }
 
   type User {
+    id: ID!
     username: String!
   }
 `;
 
-const resolvers = {
+interface User {
+  id: string;
+  username: string;
+}
+interface Users {
+  [key: string]: User;
+}
+
+const users: Users = {
+  "1": { id: "1", username: "mkubara" },
+  "2": { id: "2", username: "suzukalight" }
+};
+const me = users[1];
+
+const resolvers: IResolvers = {
   Query: {
-    me: () => ({ username: "mkubara" })
+    me: () => me,
+    users: () => Object.values(users),
+    user: (parent, { id }) => users[id] || null
   }
 };
 
