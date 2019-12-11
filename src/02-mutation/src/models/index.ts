f9b42a6a7d1f59b646514bb5cda752f92a69796a
suzukalight <mail@mkubara.com>
2019-12-11 23:26:20 +0900

    Split models

---
+++ src/02-mutation/src/models/index.ts
@@ -0,0 +1,15 @@
+export let users: Users = {
+  '1': { id: '1', firstName: 'masahiko', lastName: 'kubara' },
+  '2': { id: '2', firstName: 'suzuka', lastName: 'light' },
+};
+
+export let messages: Messages = {
+  '1': { id: '1', text: 'Hello, world!', userId: '1' },
+  '2': { id: '2', text: 'from GraphQL and Apollo-Server.', userId: '2' },
+  '3': { id: '3', text: 'textextext', userId: '2' },
+};
+
+export default {
+  users,
+  messages,
+};
