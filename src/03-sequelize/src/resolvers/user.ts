36f21b4d813bac8e48ba653b0328dcf472e72e4b
suzukalight <mail@mkubara.com>
2019-12-13 00:31:21 +0900

    Update user resolver to use sequelize

--- src/03-sequelize/src/resolvers/user.ts
+++ src/03-sequelize/src/resolvers/user.ts
@@ -1,19 +1,21 @@
 import { IResolvers } from 'apollo-server-express';
-import uuidv4 from 'uuid/v4';
 
-const resolvers: IResolvers = {
+import { User } from '../models/user';
+import { ResolverContext } from './typings';
+
+const resolvers: IResolvers<User, ResolverContext> = {
   Query: {
-    me: (parent, args, { me }) => me,
-    users: (parent, args, { models }) => Object.values(models.users),
-    user: (parent, { id }, { models }) => models.users[id],
+    me: async (parent, args, { models, me }) => models.User.findByPk(me.id),
+    users: async (parent, args, { models }) => models.User.findAll(),
+    user: async (parent, { id }, { models }) => models.User.findByPk(id),
   },
 
   User: {
-    username: (user: User) => `${user.firstName} ${user.lastName}`,
-    messages: (user: User, args, { models }) =>
-      Object.values<Message>(models.messages)
-        .filter((m: Message) => +m.userId === +user.id)
-        .map((m: Message) => models.messages[m.id]),
+    username: async user => `${user.firstName} ${user.lastName}`,
+    messages: async (user, args, { models }) =>
+      models.Message.findAll({
+        where: { userId: user.id },
+      }),
   },
 };
 
