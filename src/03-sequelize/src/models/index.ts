531a441ee6856c8ae1bd3f76a7157b45e7275826
suzukalight <mail@mkubara.com>
2019-12-13 00:15:51 +0900

    Export models

--- src/03-sequelize/src/models/index.ts
+++ src/03-sequelize/src/models/index.ts
@@ -1,15 +1,14 @@
-export let users: Users = {
-  '1': { id: '1', firstName: 'masahiko', lastName: 'kubara' },
-  '2': { id: '2', firstName: 'suzuka', lastName: 'light' },
+import { sequelize } from '../infrastructure/sequelize';
+import { User } from './user';
+import { Message } from './message';
+
+export { sequelize };
+
+const models = {
+  User,
+  Message,
 };
 
-export let messages: Messages = {
-  '1': { id: '1', text: 'Hello, world!', userId: '1' },
-  '2': { id: '2', text: 'from GraphQL and Apollo-Server.', userId: '2' },
-  '3': { id: '3', text: 'textextext', userId: '2' },
-};
+export type Models = typeof models;
 
-export default {
-  users,
-  messages,
-};
+export default models;
