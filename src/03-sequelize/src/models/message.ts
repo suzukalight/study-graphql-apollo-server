99c064de4769e122be40920efa3dfd955baa3170
suzukalight <mail@mkubara.com>
2019-12-13 00:15:25 +0900

    Add model of message

---
+++ src/03-sequelize/src/models/message.ts
@@ -0,0 +1,30 @@
+import { Model, DataTypes } from 'sequelize';
+
+import { sequelize } from '../infrastructure/sequelize';
+
+export class Message extends Model {
+  public id!: number;
+  public text!: string;
+  public userId!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Message.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    text: {
+      type: DataTypes.STRING(65500),
+      allowNull: true,
+    },
+  },
+  {
+    tableName: 'messages',
+    sequelize: sequelize,
+  },
+);
