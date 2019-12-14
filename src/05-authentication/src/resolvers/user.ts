8c56b6f1514d055010c8db8890581a5d3ee4ac32
suzukalight <mail@mkubara.com>
2019-12-14 11:16:12 +0900

    Add sign-up mutation

--- src/05-authentication/src/resolvers/user.ts
+++ src/05-authentication/src/resolvers/user.ts
@@ -3,11 +3,20 @@ import { IResolvers } from 'apollo-server-express';
 import User from '../models/user';
 import { ResolverContext } from './typings';
 
+const createToken = async (user: User) => 'dummy';
+
 const resolvers: IResolvers<User, ResolverContext> = {
   Query: {
     me: async (parent, args, { models, me }) => models.User.findByPk(me.id),
     users: async (parent, args, { models }) => models.User.findAll(),
     user: async (parent, { id }, { models }) => models.User.findByPk(id),
+  },
+
+  Mutation: {
+    signUp: async (parent, { lastName, firstName, email, password }, { models }) => {
+      const user = await models.User.create({ lastName, firstName, email, password });
+      return { token: createToken(user) };
+    },
   },
 
   User: {
--- src/05-authentication/src/schema/user.ts
+++ src/05-authentication/src/schema/user.ts
@@ -7,6 +7,14 @@ const schema = gql`
     user(id: ID!): User
   }
 
+  extend type Mutation {
+    signUp(lastName: String!, firstName: String!, email: String!, password: String!): Token!
+  }
+
+  type Token {
+    token: String!
+  }
+
   type User {
     id: ID!
     username: String!
