d5d522a348387d44a332c8570b05ea67388908cd
suzukalight <mail@mkubara.com>
2019-12-15 19:35:27 +0900

    Add page-info and connection schema to message

--- src/08-pagination/src/schema/message.ts
+++ src/08-pagination/src/schema/message.ts
@@ -2,13 +2,22 @@ import { gql } from 'apollo-server-express';
 
 const schema = gql`
   extend type Query {
-    messages(offset: Int, limit: Int): [Message!]!
+    messages(cursor: String, limit: Int): MessageConnection!
     message(id: ID!): Message!
   }
 
   extend type Mutation {
     createMessage(text: String!): Message!
     deleteMessage(id: ID!): Boolean!
+  }
+
+  type MessageConnection {
+    edges: [Message!]!
+    pageInfo: PageInfo!
+  }
+
+  type PageInfo {
+    endCursor: Date!
   }
 
   type Message {
