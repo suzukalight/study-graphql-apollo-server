0c4be43f4e377ae86ec9acb47c71448758060b11
suzukalight <mail@mkubara.com>
2019-12-22 18:28:10 +0900

    Use node_env to select database

--- src/10-testing/src/infrastructure/sequelize.ts
+++ src/10-testing/src/infrastructure/sequelize.ts
@@ -1,7 +1,12 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const storage = process.env.NODE_ENV === 'test' ? './testdatabase.sqlite' : './database.sqlite';
 
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: process.env.TEST_DATABASE || './database.sqlite',
+  storage,
   logging: false,
 });
