0d6b999ca331aa94e569ebb49496aa6a926e202b
suzukalight <mail@mkubara.com>
2019-12-27 14:55:41 +0900

    Refactor

--- src/10-testing/src/utils/test/db.ts
+++
@@ -1,7 +0,0 @@
-import models, { sequelize } from '../../models';
-import { createUsersWithMessages } from '../../seed';
-
-export const reset = async () => {
-  await sequelize.sync({ force: true });
-  await createUsersWithMessages(models);
-};
