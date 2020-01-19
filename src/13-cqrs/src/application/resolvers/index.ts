a4a3e5798456a0a007aa4a83d180efdfcd79a0df
suzukalight <mail@mkubara.com>
2020-01-19 23:55:06 +0900

    Refactor root resolvers

--- src/13-cqrs/src/application/resolvers/index.ts
+++ src/13-cqrs/src/application/resolvers/index.ts
@@ -1,14 +1,11 @@
 import { GraphQLDateTime } from 'graphql-iso-date';
 
-import userResolvers from './user';
-// import messageResolvers from './message';
-
 import Query from './query';
 import Mutation from './mutation';
-import { schemaType } from './resolvers';
+import Resolvers from './resolvers';
 
 const customScalarResolver = {
   Date: GraphQLDateTime,
 };
 
-export default [customScalarResolver, userResolvers, Query, Mutation, schemaType];
+export default [customScalarResolver, Query, Mutation, Resolvers];
