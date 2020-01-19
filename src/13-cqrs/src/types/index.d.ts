075c25d35fc1e05d8bdd5235552431780d53ce3c
suzukalight <mail@mkubara.com>
2020-01-19 23:53:40 +0900

    Add types

--- src/13-cqrs/src/types/index.d.ts
+++ src/13-cqrs/src/types/index.d.ts
@@ -2,3 +2,10 @@ type PageInfo = {
   cursor?: string;
   limit?: number;
 };
+
+type ResolverOptions = Record<string, any>;
+type FindAllOptions = ResolverOptions;
+
+type FindOneOptions = {
+  id: number;
+};
